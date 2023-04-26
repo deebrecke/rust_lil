729ef4764c0eec69a8558f3ac2606bd833fe20a2
deebrecke <brecke.deirdre@student.greenriver.edu>
2023-04-26 08:54:20 -0700

    Added accociated function

--- chapter10/src/main.rs
+++ chapter10/src/main.rs
@@ -14,17 +14,24 @@ impl Shuttle {
     fn add_fuel(&mut self, gallons: f64) {
         self.propellant += gallons
     }
+
+    fn new(name: &str) -> Shuttle {
+        Shuttle {
+            name: String::from(name),
+            crew_size: 7,
+            propellant: 0.0,
+        }
+    }
 }
 fn main() {
-    let mut vehicle = Shuttle {
-        name: String::from("Endeavour"),
-        crew_size: 7,
-        propellant: 0.0,
-    };
+    let mut vehicle = Shuttle::new("Endeavour");
+    let mut vehicle2 = Shuttle::new("Challenger");
 
     let vehicle_name = vehicle.get_name();
     println!("vehicle_name is {}", vehicle_name);
     println!("propellant is {}", vehicle.propellant);
     vehicle.add_fuel(1000.0);
     println!("propellant is now {}", vehicle.propellant);
+    vehicle2.add_fuel(500.0);
+    println!("vehicle 2 propellant: {}", vehicle2.propellant);
 }
