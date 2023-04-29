648cbe3ebdffd88373ee379eff2eb50108bd2312
deebrecke <brecke.deirdre@student.greenriver.edu>
2023-04-29 10:59:50 -0700

    Section 2 generic method definititons

--- chapter_11/src/main.rs
+++ chapter_11/src/main.rs
@@ -7,11 +7,25 @@ struct Rectangle<T, U> {
     height: U,
 }
 
+impl<T, U> Rectangle<T, U> {
+    fn get_width(&self) -> &T {
+        &self.width
+    }
+}
+
+impl Rectangle<u8, u8> {
+    fn get_perimeter(&self) -> u8 {
+        2 * self.width + 2 * self.height
+    }
+}
+
 fn main() {
     let rect = Rectangle {
-        width: 1.0,
-        height: 3,
+        width: 1,    //not specified u8--okay because 1 can be represented as a u8
+        height: 3u8, //specified is okay too, but must be the same type as above: 3u16 will NOT work
     };
     println!("rect is {:?}", rect);
+    println!("Width: {}", rect.get_width());
+    println!("Perimeter: {}", rect.get_perimeter());
 }
-//Generrics are a zero-cost abstraction
+//Generics are a zero-cost abstraction
