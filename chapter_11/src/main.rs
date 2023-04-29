c9e95139a891cd2d45b1ad0caa5758d361ed26ac
deebrecke <brecke.deirdre@student.greenriver.edu>
2023-04-29 11:05:59 -0700

    Section 3 generic function definitions

--- chapter_11/src/main.rs
+++ chapter_11/src/main.rs
@@ -1,31 +1,12 @@
-//Generic Data Types
-// use angle bracket to define generic type:
-#[derive(Debug)]
-struct Rectangle<T, U> {
-    //multiple types require multiple generics
-    width: T,
-    height: U,
-}
-
-impl<T, U> Rectangle<T, U> {
-    fn get_width(&self) -> &T {
-        &self.width
-    }
-}
-
-impl Rectangle<u8, u8> {
-    fn get_perimeter(&self) -> u8 {
-        2 * self.width + 2 * self.height
+fn get_biggest<T: PartialOrd>(a: T, b: T) -> T {
+    if a > b {
+        a
+    } else {
+        b
     }
 }
 
 fn main() {
-    let rect = Rectangle {
-        width: 1,    //not specified u8--okay because 1 can be represented as a u8
-        height: 3u8, //specified is okay too, but must be the same type as above: 3u16 will NOT work
-    };
-    println!("rect is {:?}", rect);
-    println!("Width: {}", rect.get_width());
-    println!("Perimeter: {}", rect.get_perimeter());
+    println!("biggest is {}", get_biggest(1, 2));
+    println!("biggest is {}", get_biggest(1.5, 2.3));
 }
-//Generics are a zero-cost abstraction
