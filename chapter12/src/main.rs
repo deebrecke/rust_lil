338bc6eef85b2b21ca5c32d536ba4e3984f61cae
deebrecke <brecke.deirdre@student.greenriver.edu>
2023-05-06 15:21:29 -0700

    Chapter 12.3 finished

--- chapter12/src/main.rs
+++ chapter12/src/main.rs
@@ -1,28 +1,7 @@
+#[derive(PartialEq, PartialOrd)]
 struct Satellite {
     name: String,
     velocity: f64 // miles per second
-}
-
-struct SpaceStation {
-    name: String,
-    crew_size: u8,
-    altitude: u32 // miles
-}
-
-trait Description {
-    fn describe(&self) -> String{
-        String::from("an object flying through space.")
-    }
-}
-
-impl Description for Satellite {
-
-}
-
-impl Description for SpaceStation {
-    fn describe(&self) -> String {
-        format!("the {} flying {} miles high with {} crew members aboard!", self.name, self.altitude, self.crew_size)
-    }
 }
 
 fn main() {
@@ -30,11 +9,11 @@ fn main() {
         name: String::from("Hubble Telescope"),
         velocity: 4.72
     };
-    let iss = SpaceStation {
-        name: String::from("International Space Station"),
-        crew_size: 6,
-        altitude: 254
+    let gps = Satellite {
+        name: String::from("GPS"),
+        velocity: 2.42
     };
-    println!("hubble is {}", hubble.describe());
-    println!("iss is {}", iss.describe());
+    println!("hubble == gps is {}", hubble == gps);
+    println!("hubble > gps is {}", hubble > gps);
 }
+
